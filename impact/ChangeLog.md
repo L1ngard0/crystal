b94307cfd1b906d9e354e55375e38f56f625681d
Vitaliy <ksimonj@yandex.ru>
2021-12-07 12:51:04 +0300

    commit

---
+++ impact/ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for impact
+
+## Unreleased changes
---
+++ impact/src/Lib.hs
@@ -0,0 +1,8 @@
+module Lib
+    ( someFunc
+    ) where
+
+import Data.String.Utils
+
+someFunc :: IO ()
+someFunc = putStrLn "someFunc"
